c7e7a5b97aa0bf8c257bc93e07b5187f136db264
Filip Pavlovic <fpavlovic86@gmail.com>
2020-07-08 16:29:28 +0200

    Initial commit. Creates class Car

---
+++ src/main/java/com/mycompany/factory/Car.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.factory;
+
+
+public class Car {
+    
+    String model;
+    int buildYear;
+    String color;
+    int maxSpeed;
+    int ccm;
+    int consumption;
+    
+    
+}
